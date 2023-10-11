0d4a9e625c722a1f3d23236250c8f8ca3cb089b7
myblog <khasamutdinova_e@iuca.kg>
2023-10-11 17:19:17 +0600

    First commit

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,84 @@
-from django.shortcuts import render
+from django.shortcuts import redirect
+from typing import Any,Dict
 from django.utils import timezone
-from .models import Post
+from .models import Post, Category
+from django.shortcuts import render, get_object_or_404
+from .forms import PostForm
+from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 
-def post_list(request):
-    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {'posts': posts})
+# def post_list(request):
+#     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+#     return render(request, 'blog/post_list.html', {'posts': posts})
 
-# Create your views here.
+# def post_detail(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     return render(request, 'blog/post_detail.html', {'post': post})
+
+
+# def post_new(request):
+#     if request.method == "POST":
+#         form = PostForm(request.POST)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm()
+#     return render(request, 'blog/post_edit.html', {'form': form})
+
+
+# def post_edit(request, pk):
+#     post = get_object_or_404(Post, pk=pk)
+#     if request.method == "POST":
+#         form = PostForm(request.POST, instance=post)
+#         if form.is_valid():
+#             post = form.save(commit=False)
+#             post.author = request.user
+#             post.published_date = timezone.now()
+#             post.save()
+#             return redirect('post_detail', pk=post.pk)
+#     else:
+#         form = PostForm(instance=post)
+#     return render(request, 'blog/post_edit.html', {'form': form})
+# # Create your views here.
+
+# # from django.views.generic import View
+
+# # class PostDetail(View):
+# #     def get(self, request, slug):
+# #         post = Post.objects.get(slug__iexact=slug)
+# #         return render(request, 'blog/post_detail.html', {'post': post})
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'blog/category_list.html', {'categories': categories})
+
+class PostCreateView(LoginRequiredMixin,CreateView):
+    template_name = 'blog/post_create.html'
+    form_class = PostForm
+    permission_required = ('news.add_post')
+    raise_exception = True
+
+    def form_valid(self, form):
+        fields = form.save(commit=False)
+        fields.post_author = self.request.user
+        fields.save()
+        return super().form_valid(form)
+    
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name ='post'
+
+class PostUpdateView(UpdateView):
+    model = Post
+    fields = ["title", "text"]
+    template_name = 'blog/post_edit.html'
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'    
